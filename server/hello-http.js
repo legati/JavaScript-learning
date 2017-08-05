5a9d16cd430c061a24107885de37ba97636c877f
Nick Klymenko <nick.klymen@gmail.com>
2017-08-05 13:46:26 +0800

    Server time and date added

--- server/hello-http.js
+++ server/hello-http.js
@@ -1,8 +1,13 @@
 var http = require('http');
 
+var currentdate = new Date();
+var datetime = currentdate.getDate() + "/"+ (currentdate.getMonth() + 1) + "/" 
++ currentdate.getFullYear() + " @ " + currentdate.getHours() + ":" 
++ currentdate.getMinutes() + ":" + currentdate.getSeconds();
+
 var server = http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type':'text/plain'});
-    response.end('Hello, world');
+    response.end('Hello, world' + '\n' + datetime);
 });
 
 //Listen to port localhost:8000
