41b0185ed8a038afa38bf86f14a5ddbeabd2f0c4
legati <nick.klymen@gmail.com>
2017-09-04 16:07:58 +0800

    ''

--- server/hello-http.js
+++ server/hello-http.js
@@ -56,13 +56,14 @@ var server = http.createServer(function (request, response) {
     response.write('Hello, world' + '\n' + currentdate + '\n' + Math.random() + '\n');
 
     MongoClient.connect(url, function(err, db){
-        if (err) console.error(err);
-        db.collection('customers').find({ _id: 154}).toArray(function(err, results) {
-                if (err) console.error(err);
+        if (err) throw err;
+        var myQuery = {_id: 154};
+        db.collection('customers').find(myQuery).toArray(function(err, results) {
+                if (err) throw (err);
                 var dbRes = String(results[0].name);
                 response.write(dbRes + '\n');
                 googleSearchUrl(err, dbRes, function finishedSearch(err, searchOut) {
-                        if (err) console.error(err);
+                        if (err) throw (err);
                         response.write('test \n' + String(searchOut));
                         insertDocuments(db, searchOut, function() {
                                 db.close();
